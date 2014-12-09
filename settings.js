2188794fe0cada27b5313640592d2ede51f9908d
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-12-09 11:09:58 +0100

    feat(dest): New build files.

--- settings.js
+++ settings.js
@@ -7,12 +7,17 @@
 
 angular.module( '4screens.settings', [] );
 
-angular.module('4screens.settings').factory( 'SettingsService',
-  function() {
+'use strict';
+
+angular.module('4screens.settings').factory( 'SettingsSocialhubService',
+  ["$http", "CONFIG", function( $http, CONFIG ) {
     return {
-      hello: function() {
-        return 'world';
+      get: function( widgetId ) {
+        CONFIG.frontend.socialhubWidget.id = widgetId;
+        return $http.get( CONFIG.backend.domain + CONFIG.frontend.socialhubWidget.get.replace( ':widgetId', widgetId ) ).then(function( res ) {
+          return res.data;
+        });
       }
     };
-  }
+  }]
 );
