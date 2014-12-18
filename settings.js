54a7f1820a6732f173706a7e52975e0d168a2583
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-12-18 12:26:27 +0100

    feat(dest): New build files.

--- settings.js
+++ settings.js
@@ -9,6 +9,48 @@ angular.module( '4screens.settings', [] );
 
 'use strict';
 
+angular.module('4screens.settings').factory( 'SettingsEngageformService',
+  ["$http", "CONFIG", function( $http, CONFIG ) {
+    return {
+      get: function( engageFormId ) {
+        if( !engageFormId ) {
+          throw 'engageFormId has not been set!';
+        }
+
+        return $http.get( CONFIG.backend.answers.domain + CONFIG.backend.answers.getQuizUrl.replace( ':quizId', engageFormId ) ).then(function( res ) {
+          return res.data;
+        });
+      },
+      sendAnswer: function( response ) {
+        if( !response ) {
+          throw 'response has not been set!';
+        }
+        if( !response.quizQuestionId ) {
+          throw 'response quizQuestionId has not been set!';
+        }
+
+        return $http.post(
+          CONFIG.backend.answers.domain + CONFIG.backend.answers.answerUrl.replace( ':questionId', response.quizQuestionId ),
+          response
+        ).then(function( res ) {
+          return res.data;
+        });
+      },
+      getQuestions: function( engageFormId ) {
+        if( !engageFormId ) {
+          throw 'engageFormId has not been set!';
+        }
+
+        return $http.get( CONFIG.backend.answers.domain + CONFIG.backend.answers.getQuestionsUrl.replace( ':quizId', engageFormId ) ).then(function( res ) {
+          return res.data;
+        });
+      }
+    };
+  }]
+);
+
+'use strict';
+
 angular.module('4screens.settings').factory( 'SettingsSocialhubService',
   ["$http", "CONFIG", function( $http, CONFIG ) {
     return {
